2e43acdcd7b4ead43632967a8698bd2e0b8ef956
Mirko <131188762+Mirunelo@users.noreply.github.com>
2023-04-18 23:17:46 +0200

    19 Metode - Zadaci
    Metode - Zadaci

---
+++ 19_metode_zadaci/src/canak_mirko/Primer14.java
@@ -0,0 +1,16 @@
+package canak_mirko;
+
+public class Primer14 {
+	/* Metoda za prevođenje Celzijusovih u Farenthajtove stepene */
+	public static double CuF(double celsius) {
+		return (1.8 * celsius + 32);
+	}
+
+	public static void main(String[] args) throws Exception {
+		/* Izmerena temperatura */
+		double temp = 25;
+		/* Štampanje rezultata */
+		System.out.println("Temperatura prevedena u Farenthajte iznosi: " + CuF(temp));
+
+	}
+}
