0e5ea00b389b3d57dd34e8a69dffe012cc72571c
Mirko <131188762+Mirunelo@users.noreply.github.com>
2023-04-19 00:08:42 +0200

    Delete 21_klase_objekti directory

--- 21_klase_objekti/02_instance_of/src/canak_mirko/ABC.java
+++
@@ -1,9 +0,0 @@
-package canak_mirko;
-
-public class ABC {
-
-	ABC() {
-		System.out.println("Objekat a je kreiran.");
-	}
-
-}
--- 21_klase_objekti/02_instance_of/src/canak_mirko/XYZ.java
+++
@@ -1,8 +0,0 @@
-package canak_mirko;
-
-public class XYZ {
-
-	XYZ() {
-		System.out.println("Objekat x je kreiran.");
-	}
-}
