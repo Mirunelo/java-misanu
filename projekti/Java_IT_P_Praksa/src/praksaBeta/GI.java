291b0a617380ffb68cd6993a60c1e144efb088ce
Mirko <mirunelo@gmail.com>
2023-04-29 12:34:55 +0200

    Projekti

--- projekti/Java_IT_P_Praksa/src/praksaBeta/GI.java
+++ projekti/Java_IT_P_Praksa/src/praksaBeta/GI.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 
 public interface GI {
 
-	public default void poruka(String string1, String string2) {
-		JOptionPane.showMessageDialog(null, string2, string1, JOptionPane.INFORMATION_MESSAGE);
+	public default void poruka(String tipPoruke, String poruka) {
+		JOptionPane.showMessageDialog(null, poruka, tipPoruke, JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 }
